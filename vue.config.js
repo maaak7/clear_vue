79950dddaf4624cecb2472730828a08639fead19
maaak7 <maaak7ka@gmail.com>
2021-01-06 18:14:26 +0200

    eslint removed

--- vue.config.js
+++ vue.config.js
@@ -14,15 +14,6 @@ module.exports = {
       }
     }
   },
-  chainWebpack: (config) => {
-    config.module
-      .rule("eslint")
-      .use("eslint-loader")
-      .tap((options) => {
-        options.configFile = path.resolve(__dirname, ".eslintrc.js");
-        return options;
-      });
-  },
   css: {
     loaderOptions: {
       postcss: {
